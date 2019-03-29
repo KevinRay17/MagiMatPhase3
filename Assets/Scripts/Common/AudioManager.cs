6f03de3b206b799b7797d89b47be07916630ba10
Chazzmcfreely <cwh360@nyu.edu>
2019-03-29 14:45:15 -0400

    sounds and scripts

--- Assets/Scripts/Common/AudioManager.cs
+++ Assets/Scripts/Common/AudioManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine.Audio;
 using UnityEngine;
 
+// Attach this script to an empty game object in the scene called Audior
 public class AudioManager : MonoBehaviour
 {
     // references this script possibly, not 100% sure about instances
@@ -19,9 +20,14 @@ public class AudioManager : MonoBehaviour
 
     public void Awake()
     {
-        tracks = new AudioSource[numberOfAudioTracks];
+        tracks = new AudioSource[numberOfAudioTracks]; //intializes the array of AudioSources
+        currentTrackNumber = 0; //number used for navigating the tracks array
 
         
+        //create an empty game object for every audiotrack
+        //parent it to the Audio gameobject
+        //add an audiosource
+        //Assign that audiosource to the i'th element of the tracks array
         for (int i = 0; i < numberOfAudioTracks; i++)
         {
             var GO = new GameObject("Track" + (i + 1));
@@ -32,6 +38,15 @@ public class AudioManager : MonoBehaviour
 
 
         instance = this;
+    }
+
+    
+    //function to call from other scripts when a sound needs to be played 
+    //plays sound on one of the many audiosources then moves up the list of audiosources
+    void playSound(AudioClip clip)
+    {
+        tracks[currentTrackNumber].PlayOneShot(clip);
+        currentTrackNumber++;
     }
 
 
