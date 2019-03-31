a579259f3123ce974f1a8d4209fac0ca5eef9005
Jackson Cella <cjc771@nyu.edu>
2019-03-31 17:55:16 -0400

    Added PlayerSpriteManager.cs , and made changes to PlayerMovement.cs

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -9,6 +9,8 @@ public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
 
+    public PlayerSpriteManager animate;
+
     private Vector2 _inputVector;
 
     [HideInInspector] public int faceDirection; //1 = UP, 2 = RIGHT, 3 = DOWN, 4 = LEFT
@@ -43,6 +45,8 @@ public class PlayerMovement : MonoBehaviour
         //store original gravity scale in case it is changed later
         gravityScale = _rigidbody2D.gravityScale;
 
+        
+
         canMove = true;
     }
 
@@ -71,6 +75,7 @@ public class PlayerMovement : MonoBehaviour
             }
             Jump(jumpPower);
            hasJumped = true;
+            animate.AnimatePlayerJump();
         }
 
         //if the player is not currently climbing, circleCast nearby to look for climbables
---
+++ Assets/Scripts/Player/PlayerSpriteManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpriteManager : MonoBehaviour
+{
+
+    private SpriteRenderer playerSprite;
+
+    public void Start(){
+       playerSprite = GetComponent<SpriteRenderer>();
+    }
+    
+    //TO ADD ANY ANIMATION TO ANYTHING IN PLAYERMOVEMENT.CS
+    // USE:   animate.FunctionNameInThisScript();
+    
+    public void AnimatePlayerMoveRight()
+    {
+        //Replace this with animation
+       
+    }
+    public void AnimatePlayerMoveLeft()
+    {
+        //Replace this with animation
+       
+    }
+    public void AnimatePlayerDashRight()
+    {
+        //replace this with animation
+       
+    }
+    public void AnimatePlayerDashLeft()
+    {
+        //replace this with animation
+       
+    }
+    public void AnimatePlayerJump()
+    {
+        //replace this with animation
+       
+    }
+}
