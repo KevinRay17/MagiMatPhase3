0087d75ad945241342e4ea9a1c08134eb6340192
Kevin Ray <kr1995@nyu.edu>
2019-04-03 15:44:02 -0400

    Fire Unlock, Camera Tiggers, MaterialManager Prefab

---
+++ Assets/Scripts/Player/EventTriggers.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.UIElements.GraphView;
+using UnityEngine;
+
+public class EventTriggers : MonoBehaviour
+{
+    public Camera camera;
+
+    private float _targetScale = 6.4f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (camera.orthographicSize != _targetScale)
+        {
+            camera.orthographicSize += (_targetScale - camera.orthographicSize) * .05f;
+        }
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("camTrigger"))
+        {
+            _targetScale = 15f;
+        } else if (other.gameObject.CompareTag("camResetScale"))
+        {
+            _targetScale = 6.4f;
+        }
+    } 
+    
+}
