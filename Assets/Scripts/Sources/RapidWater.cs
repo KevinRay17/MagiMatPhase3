06700865f05f214429c3c8fb5b761dbecb1cde9b
Chazzmcfreely <cwh360@nyu.edu>
2019-04-14 16:34:11 -0400

    stuff from my class work

---
+++ Assets/Scripts/Sources/RapidWater.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RapidWater : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerStay2D(Collider2D player)
+    {
+        Debug.Log("Your Drowning");
+        player.GetComponent<Rigidbody2D>().AddForce(Vector2.left * 100);
+    }
+}
