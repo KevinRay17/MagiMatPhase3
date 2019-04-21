37ff4e48fa716118c2d4544ffb9f2e2298c00d57
Jackson Cella <cjc771@nyu.edu>
2019-04-21 17:38:02 -0400

    Main menu scene

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public CameraScript cameraScriptVariable;
+    void Start()
+    {
+        cameraScriptVariable.enabled = false;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
