f8f6811918dcb4c7fc565a501b46fa55064ef1f0
Chazzmcfreely <cwh360@nyu.edu>
2019-04-21 18:10:06 -0400

    Rock Tutorial Scene

---
+++ Assets/Scripts/timerDoor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timerDoor : MonoBehaviour
+{
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (button1.gameObject.GetComponent<timerButtons>().isActive == true && button3.gameObject.GetComponent<timerButtons>().isActive && button1.gameObject.GetComponent<timerButtons>().isActive)
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
